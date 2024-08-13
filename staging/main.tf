cf7210d993faeeeba42550bb8d79945cc5351dc6
Alex Chang <achang209@gmail.com>
2024-08-13 16:47:47 -0500

    correction

--- staging/main.tf
+++ staging/main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-east-2"
 }
 
-resource "aws_instanc" "staging_example" {
+resource "aws_instance" "staging_example" {
   ami = "ami-0862be96e41dcbf74"
   instance_type = "t2.micro"
   tags = {
