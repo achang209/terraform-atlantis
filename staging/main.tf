c0dce22fde4af730ad24d1cc88e02f01d1e646bd
Alex Chang <achang209@gmail.com>
2024-08-13 09:01:08 -0500

    intentional break

--- staging/main.tf
+++ staging/main.tf
@@ -2,9 +2,9 @@ provider "aws" {
   region = "us-east-2"
 }
 
-resource "aws_instance" "staging_example" {
+resource "aws_instanc" "staging_example" {
   ami = "ami-0862be96e41dcbf74"
-  instance_type = "t2.microg"
+  instance_type = "t2.micro"
   tags = {
     Name = "staging-example"
   }
