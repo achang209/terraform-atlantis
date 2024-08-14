013df3b090d672e91f437f27f1805873f7c346a5
Alex Chang <achang209@gmail.com>
2024-08-14 11:17:44 -0500

    basic workflow

--- staging/main.tf
+++ staging/main.tf
@@ -2,13 +2,13 @@ provider "aws" {
   region = "us-east-2"
 }
 
-# resource "aws_instance" "staging_example" {
-#   ami = "ami-0862be96e41dcbf74"
-#   instance_type = "t2.micro"
-#   tags = {
-#     Name = "staging-example"
-#   }
-# }
+resource "aws_instanc" "staging_example" {
+  ami = "ami-0862be96e41dcbf74"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "staging-example"
+  }
+}
 
 # resource "aws_security_group" "sg_allow_http" { 
 #   name = "allow-wide-open-http-sg"
