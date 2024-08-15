f574af820b3e47b8b4d302e19482655413737936
Alex Chang <achang209@gmail.com>
2024-08-15 11:37:00 -0500

    destroy resources

--- staging/main.tf
+++ staging/main.tf
@@ -2,13 +2,13 @@ provider "aws" {
   region = "us-east-2"
 }
 
-resource "aws_instance" "staging_example" {
-  ami = "ami-0862be96e41dcbf74"
-  instance_type = "t2.micro"
-  tags = {
-    Name = "staging-no-approval"
-  }
-}
+# resource "aws_instance" "staging_example" {
+#   ami = "ami-0862be96e41dcbf74"
+#   instance_type = "t2.micro"
+#   tags = {
+#     Name = "staging-no-approval"
+#   }
+# }
 
 # resource "aws_security_group" "sg_allow_http" { 
 #   name = "allow-wide-open-http-sg"
