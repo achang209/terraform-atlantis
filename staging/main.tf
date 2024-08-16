289afdbf978c0c05f030cb01a6c6fd8c958c71c1
Alex Chang <achang209@gmail.com>
2024-08-15 20:23:10 -0500

    reset

--- staging/main.tf
+++ staging/main.tf
@@ -2,14 +2,14 @@ provider "aws" {
   region = "us-east-2"
 }
 
-resource "aws_instance" "staging_example" {
-  # ami = "ami-003932de22c285676"
-  ami = "ami-0862be96e41dcbf74"
-  instance_type = "t2.medium"
-  tags = {
-    Name = "staging-no-approvals"
-  }
-}
+# resource "aws_instance" "staging_example" {
+#   ami = "ami-003932de22c285676"
+#   # ami = "ami-0862be96e41dcbf74"
+#   instance_type = "t2.micro"
+#   tags = {
+#     Name = "staging-example"
+#   }
+# }
 
 # resource "aws_security_group" "sg_allow_http" { 
 #   name = "allow-wide-open-http-sg"
